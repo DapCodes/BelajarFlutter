1857fc171a790e2520702a8c3938bf1a01c2a774
DapCodes <daffaramadhan929@gmail.com>
2025-07-28 16:49:09 +0700

    Flutter Fundamentals

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:xii_rpl_1/main_layout.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HelloFlutter());
+  }
+}
+
+class HelloFlutter extends StatelessWidget {
+  const HelloFlutter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MainLayout(
+      title: 'Belajar Flutter',
+      body: Center(
+        child: Text(
+          'Hello FLutter',
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            backgroundColor: Colors.blueAccent,
+          ),
+        ),
+      ),
+    );
+  }
+}
